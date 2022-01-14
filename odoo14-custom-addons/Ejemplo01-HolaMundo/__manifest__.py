360e6b69e8c8bb9a94259c06b769f3db2d390cba
Rodrigo <rodrigo.zafra2@educa.madrid.org>
2022-01-14 08:46:42 +0100

    initial commit

---
+++ odoo14-custom-addons/Ejemplo01-HolaMundo/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{'name': "Ejemplo01-Hola mundo",
+
+    'summary': """Primer módulo Odoo""",
+
+    'description': """
+        Eres un calamorro y no hay quien te doble la joroba.
+    """,
+
+    'author': "Rodrigo&Mario Associate Corporation",
+    'website': "http://www.associate.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1'
+}
