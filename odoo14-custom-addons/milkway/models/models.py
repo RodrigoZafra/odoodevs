d5c2dd73f92815b213c1f44c093ca8a93b6d4586
Rodrigo <rodrifdt17@gmail.com>
2022-03-01 12:07:16 +0100

    milkway terminado

--- odoo14-custom-addons/milkway/models/models.py
+++ odoo14-custom-addons/milkway/models/models.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class milkway(models.Model):
-#     _name = 'milkway.milkway'
-#     _description = 'milkway.milkway'
+class milkway(models.Model):
+    _name = 'milkway.milkway'
+    _description = 'milkway.milkway'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    planet_status = fields.Text() 
+    mass = fields.Float()
+    radius = fields.Text()
+    orbital_period = fields.Float()
+    angular_distance = fields.Text()
+    discovered = fields.Integer()
+    molecules = fields.Text()
+    star_name = fields.Text()
+    star_distance = fields.Float() 
+    star_age = fields.Float()
+    
+ 
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
